8bdb6248d9e6632aea2b6a8592682a1eeb41009f
mickaelchau <mickaelchau4@gmail.com>
2022-03-11 12:38:10 +0100

    filteredLogger no longer exists

--- src/main/java/fr/lernejo/logger/LoggerFactory.java
+++ src/main/java/fr/lernejo/logger/LoggerFactory.java
@@ -1,18 +1,14 @@
 package fr.lernejo.logger;
 
-import java.util.function.Predicate;
-
 public class LoggerFactory {
     public static Logger getLogger(String name) {
         FileLogger newFileLogger = new FileLogger("log.txt");
         ContextualLogger newContextualLogger1 = new ContextualLogger(newFileLogger, name);
-        Predicate<String> containsSimulation = x -> x.contains("simulation");
-        FilteredLogger newFilteredLogger = new FilteredLogger(newContextualLogger1, containsSimulation);
         
         ConsoleLogger newConsoleLogger = new ConsoleLogger(); 
         ContextualLogger newContextualLogger2 = new ContextualLogger(newConsoleLogger, name);
 
-        CompositeLogger newCompositeLogger = new CompositeLogger(newFilteredLogger, newContextualLogger2);
+        CompositeLogger newCompositeLogger = new CompositeLogger(newContextualLogger1, newContextualLogger2);
         return newCompositeLogger;
     }
 }
