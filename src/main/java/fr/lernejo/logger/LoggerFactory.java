9e2be79bd21baa6efe61c0a4302a53a0d9582b97
mickaelchau <mickaelchau4@gmail.com>
2022-03-11 14:40:27 +0100

    all Loggers type are now Logger

--- src/main/java/fr/lernejo/logger/LoggerFactory.java
+++ src/main/java/fr/lernejo/logger/LoggerFactory.java
@@ -4,15 +4,15 @@ import java.util.function.Predicate;
 
 public class LoggerFactory {
     public static Logger getLogger(String name) {
-        FileLogger newFileLogger = new FileLogger("log.txt");
+        Logger newFileLogger = new FileLogger("log.txt");
         Logger newContextualLogger1 = new ContextualLogger(newFileLogger, name);
         Predicate<String> containsSimulation = x -> x.contains("simulation");
         FilteredLogger newFilteredLogger = new FilteredLogger(newContextualLogger1, containsSimulation);
         
-        ConsoleLogger newConsoleLogger = new ConsoleLogger(); 
-        ContextualLogger newContextualLogger2 = new ContextualLogger(newConsoleLogger, name);
+        Logger newConsoleLogger = new ConsoleLogger(); 
+        Logger newContextualLogger2 = new ContextualLogger(newConsoleLogger, name);
 
-        CompositeLogger newCompositeLogger = new CompositeLogger(newFilteredLogger, newContextualLogger2);
+        Logger newCompositeLogger = new CompositeLogger(newFilteredLogger, newContextualLogger2);
         return newCompositeLogger;
     }
 }
