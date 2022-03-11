850f5440397fb31136e4b6f8055771aec8235543
mickaelchau <mickaelchau4@gmail.com>
2022-03-11 10:31:07 +0100

    Part7

--- src/main/java/fr/lernejo/logger/LoggerFactory.java
+++ src/main/java/fr/lernejo/logger/LoggerFactory.java
@@ -1,14 +1,18 @@
 package fr.lernejo.logger;
 
-import java.io.Console;
+import java.util.function.Predicate;
 
 public class LoggerFactory {
     public static Logger getLogger(String name) {
         FileLogger newFileLogger = new FileLogger("log.txt");
-        ConsoleLogger newConsoleLogger = new ConsoleLogger(); 
         ContextualLogger newContextualLogger1 = new ContextualLogger(newFileLogger, name);
+        Predicate<String> containsSimulation = x -> x.contains("simulation");
+        FilteredLogger newFilteredLogger = new FilteredLogger(newContextualLogger1, containsSimulation);
+        
+        ConsoleLogger newConsoleLogger = new ConsoleLogger(); 
         ContextualLogger newContextualLogger2 = new ContextualLogger(newConsoleLogger, name);
-        CompositeLogger newCompositeLogger = new CompositeLogger(newContextualLogger1, newContextualLogger2);
+
+        CompositeLogger newCompositeLogger = new CompositeLogger(newFilteredLogger, newContextualLogger2);
         return newCompositeLogger;
     }
 }
